23a3e804ab7241dd2737127dcdfb1fde4d25b790
Code Sandbox <hello@codesandbox.io>
2022-04-26 14:03:31 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.21b21f4ff943053d188d7b56aaff04f6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60cdd9bc3a8284663717c1bae0b1b848",
+    "url": "https://kuldeeptak22.github.io/csb-x9hi3q/index.html"
+  },
+  {
+    "revision": "2465a4e45eb3f8896c9e",
+    "url": "https://kuldeeptak22.github.io/csb-x9hi3q/static/js/2.5fc45a0c.chunk.js"
+  },
+  {
+    "revision": "674924619d86c370b9f9",
+    "url": "https://kuldeeptak22.github.io/csb-x9hi3q/static/js/main.50f810b5.chunk.js"
+  },
+  {
+    "revision": "9448a746971a095186f9",
+    "url": "https://kuldeeptak22.github.io/csb-x9hi3q/static/js/runtime-main.94cc68fb.js"
+  }
+]);
